cd9ac50ee825cbd2ace44e98c1d251198d5d4ca0
yocn <zhaoyingkun@163.com>
2019-05-13 18:31:44 +0800

    First Commit

---
+++ Main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import testVariable
+
+testVariable.test()
---
+++ testVariable.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+def testOperator():
+    # --------------------------String----------------------
+
+    return
